f78e8e118a81acdca55edd92446e4d2895b00be5
jnmerchant <joemercnc@gmail.com>
2017-01-20 16:33:40 -0500

    updated new rating object test

--- spec/rating_spec.rb
+++ spec/rating_spec.rb
@@ -3,8 +3,8 @@ require 'rating'
 describe Rating do
   describe ' # new ' do
     context ' #creates a new rating object' do
-      it 'creates a new rating object with an rating id, user id, movie id, rating' do
-      rating = Rating.new(1, 32, 34, 3.5)
+      it 'creates a new rating object with an user id, movie id, rating' do
+      rating = Rating.new(32, 34, 3.5)
       expect(rating.user_id).to eq 32
       expect(rating.score).to eq 3.5
       end
