a716dc68734b42673cb6681769204cefa9db2502
Jenn Tustin <jenn.tustin@gmail.com>
2017-01-21 12:23:27 -0500

    Add test for calculate average rating, start test for get_movie_rating

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,13 +1,35 @@
 require 'movie'
 
 describe Movie do
+  movie = Movie.new(1, 'Toy Story', 'Comedy')
+  ratings = 
+
   describe ' # new ' do
     context ' #creates a new movie object' do
       it 'creates a new movie object with an id, title, and genre' do
-      movie = Movie.new(1, 'Toy Story', 'Comedy')
+
       expect(movie.title).to eq 'Toy Story'
       expect(movie.id).to eq 1
       end
     end
   end
+
+  describe '#get_movie_rating' do
+    context 'when passed a hash of rating objects' do
+      it 'gets an array of all ratings for movie and returns an average rating' do
+
+        expect(movie.get_movie_rating(ratings))
+      end
+    end
+  end
+
+  describe ' # calculate_average_rating ' do
+    context 'when passed an array of movie ratings' do
+      it 'returns the average' do
+        matching_ratings = [2.0, 2.5, 5]
+        expect(movie.calculate_average_rating(matching_ratings)). to eq 3.2
+      end
+    end
+  end
+
 end
