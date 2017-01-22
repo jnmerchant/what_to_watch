af2c03e57fa50e998d0ac7edd9459f67bc88b6f3
jnmerchant <joemercnc@gmail.com>
2017-01-22 09:17:51 -0500

    changed reading in movies so hash key was int instead of string, changed ratings class so ratings were assigned to movies right as the file is read in, changed movie class so a movie knows all its ratings

--- lib/main.rb
+++ lib/main.rb
@@ -7,20 +7,18 @@ def main
   rating_file = '../data/rating_sample_data.csv'
 
   movies = Movie.read_file(movie_file)
-  ratings = Rating.read_file(rating_file)
+  ratings = Rating.read_file(rating_file, movies)
 
   search_prompt
   search_input = gets.chomp
-
   matching_titles = Movie.search_titles(search_input, movies)
   Movie.display_matching_titles(matching_titles)
+
 
 end
 
 def search_prompt
   puts "Enter movie title to search: "
 end
-
-
 
 main if __FILE__ == $PROGRAM_NAME
--- lib/movie.rb
+++ lib/movie.rb
@@ -1,45 +1,32 @@
 class Movie
   attr_reader :id, :title, :genre
+  attr_accessor :ratings
 
   def initialize(id, title, genre)
     @id = id
     @title = title
     @genre = genre
-    @average_rating = 0
-    # @ratings = []
+    @ratings = []
   end
 
-  # def add_rating(rating)
-  #   @ratings << rating
-  # end
-
-  def get_ratings(all_ratings)
-    matching_ratings = []
-
-    all_ratings.each do |rating_id, rating|
-      if rating.movie_id == @id
-        matching_ratings << rating.score
-      end
+  def get_average_rating
+    if @ratings.count == 0
+      average_rating = 0
+    else
+      avg_rating = (@ratings.reduce(0, :+) / @ratings.count).round(1)
+      average_rating = avg_rating
     end
-
-    matching_ratings
-  end
-
-  def get_average_rating(all_ratings)
-    ratings = get_ratings(all_ratings)
-    avg_rating = (ratings.reduce(0, :+) / ratings.count).round(1)
-    @average_rating = avg_rating
   end
 
   def to_s
-    "Title: #{@title} Genre: #{@genre} Average Rating: #{@average_rating}"
+    "Title: #{@title} Genre: #{@genre} Average Rating: #{get_average_rating}"
   end
 
   def self.read_file(movie_file)
     movies = {}
 
     CSV.foreach(movie_file, {:headers => true }) do |movie|
-      movie_id = movie[0]
+      movie_id = movie[0].to_i
       movie_obj = Movie.new(movie[0].to_i, movie[1], movie[2])
       movies[movie_id] = movie_obj
     end
